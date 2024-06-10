eb79a7c5129c18c53498a04155f1a8004b0760b2
ulysses.leal <ulyssesandradeleal@gmail.com>
2024-06-10 09:10:52 -0300

    fix:change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import path from 'path';
-import { connectToDatabase } from './db'; 
+import { connectToDatabase } from './db';
 import mongoose, { Document } from 'mongoose';
-import { z } from 'zod'; 
+import { z } from 'zod';
 
 // Definindo o modelo de endereço
 interface Endereco {
@@ -23,13 +23,13 @@ interface Pessoa extends Document {
 // Conecta ao banco de dados MongoDB
 connectToDatabase();
 
-export const app = express();
+const app: Express = express();
 app.use(cors());
 app.use(express.json());
 
 // Definindo o esquema do documento Pessoa
 const pessoaSchema = new mongoose.Schema<Pessoa>({
-  email: { type: String, unique: true }, // Adiciona a opção unique para garantir que o email seja único
+  email: { type: String, unique: true },
   nome: String,
   idade: Number,
   endereco: {
@@ -37,7 +37,7 @@ const pessoaSchema = new mongoose.Schema<Pessoa>({
     cidade: String,
     estado: String,
   }
-}, { toJSON: { virtuals: true } }); // Permitir que os subdocumentos sejam incluídos na resposta JSON
+}, { toJSON: { virtuals: true } });
 
 // Criando o modelo Pessoa
 const PessoaModel = mongoose.model<Pessoa>('Pessoa', pessoaSchema);
@@ -57,24 +57,14 @@ const pessoaInputSchema = z.object({
 app.post('/pessoa', async (req: Request, res: Response) => {
   try {
     const pessoaData = pessoaInputSchema.parse(req.body);
-    if (!pessoaData) {
-      return res.status(400).json({ message: 'Dados de pessoa inválidos' });
-    }
-
     const { email, nome, idade, endereco } = pessoaData;
 
-    // Verifica se já existe uma pessoa com o mesmo e-mail
     const existingPessoa = await PessoaModel.findOne({ email });
     if (existingPessoa) {
       return res.status(400).json({ message: 'Já existe uma pessoa com este e-mail' });
     }
 
-    const pessoa = new PessoaModel({
-      email,
-      nome,
-      idade,
-      endereco,
-    });
+    const pessoa = new PessoaModel({ email, nome, idade, endereco });
     await pessoa.save();
     const { endereco: _, ...rest } = pessoa.toObject();
     res.status(201).json(rest);
@@ -87,12 +77,9 @@ app.post('/pessoa', async (req: Request, res: Response) => {
   }
 });
 
-
-// Rota para buscar uma pessoa pelo ID
 app.get('/pessoa/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
   try {
-    const pessoa = await PessoaModel.findById(id);
+    const pessoa = await PessoaModel.findById(req.params.id);
     if (!pessoa) {
       return res.status(404).json({ message: 'Pessoa não encontrada' });
     }
@@ -102,7 +89,6 @@ app.get('/pessoa/:id', async (req: Request, res: Response) => {
   }
 });
 
-// Rota para buscar uma lista paginada de pessoas
 app.get('/pessoa', async (req: Request, res: Response) => {
   const { page = 1, pageSize = 10 } = req.query;
   const skip = (Number(page) - 1) * Number(pageSize);
@@ -114,39 +100,32 @@ app.get('/pessoa', async (req: Request, res: Response) => {
   }
 });
 
-// Rota para deletar uma pessoa pelo ID
 app.delete('/pessoa/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
   try {
-    // Buscar a pessoa antes de deletar
-    const pessoaDeletada = await PessoaModel.findByIdAndDelete(id);
+    const pessoaDeletada = await PessoaModel.findByIdAndDelete(req.params.id);
     if (!pessoaDeletada) {
       return res.status(404).json({ message: 'Pessoa não encontrada' });
     }
-
-    
-    res.status(200).json({ message: 'Pessoa deletada com sucesso'});
+    res.status(200).json({ message: 'Pessoa deletada com sucesso' });
   } catch (error) {
     res.status(500).json({ message: 'Erro ao deletar pessoa' });
   }
 });
 
-// Rota para atualizar uma pessoa pelo ID
 app.put('/pessoa/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
   try {
-    const {email, nome, idade, endereco } = req.body;
-    const pessoa = await PessoaModel.findByIdAndUpdate(id, {
-      email,
-      nome,
-      idade,
-      endereco,
-    }, { new: true });
+    const { email, nome, idade, endereco } = req.body;
+    const pessoa = await PessoaModel.findByIdAndUpdate(
+      req.params.id,
+      { email, nome, idade, endereco },
+      { new: true }
+    );
     res.json(pessoa);
   } catch (error) {
     res.status(400).json({ message: 'Erro ao atualizar pessoa' });
   }
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
